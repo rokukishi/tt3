40876674a57ab3c4981e8322297ad239e5e7e241
root <root@jessie.raw>
2018-02-15 17:04:13 +0000

    foi

--- nome.sh
+++ nome.sh
@@ -4,19 +4,19 @@ clear
 echo "Formulário"
 echo "Nome: " 
 read NOME
-echo $NOME > /tt3projeto/nome.txt
-grep -i -q '^[a-z]' /tt3projeto/nome.txt
+echo $NOME > /tt3/nome.txt
+grep -i -q '^[a-z]' /tt3/nome.txt
 case $? in
-	0) grep -i -q '[a-z]$' /tt3projeto/nome.txt; CONTEUDO=$?; f1;;
+	0) grep -i -q '[a-z]$' /tt3/nome.txt; CONTEUDO=$?; f1;;
 	1) echo "Nome inválido"; sleep 3; nome;;
 esac
 }
 f1(){
 if [ $CONTEUDO == "0" ]; then
-	grep -q '[0-9]' /tt3projeto/nome.txt
+	grep -q '[0-9]' /tt3/nome.txt
 	case $? in
 		0) echo "Nome inválido"; sleep 3; nome;;
-		1) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "Nome: $NOME" > /tt3projeto/.dados/$NOME | email;;
+		1) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "Nome: $NOME" > /tt3/.dados/$NOME; email;;
 	esac
 else
 	echo "Nome inválido"
@@ -29,23 +29,23 @@ clear
 echo $NOME
 echo "E-mail: "
 read EMAIL
-echo $EMAIL > /tt3projeto/email.txt
-grep -q '[A-Z]' /tt3projeto/email.txt
+echo $EMAIL > /tt3/email.txt
+grep -q '[A-Z]' /tt3/email.txt
 if [ $? = "0" ]; then
         echo "E-mail inválido"
         sleep 3
         email
 else
-        grep -q '^[0-9]' /tt3projeto/email.txt
+        grep -q '^[0-9]' /tt3/email.txt
         case $? in
                 0) echo "E-mail inválido"; sleep 3; email;;
-                1) grep -q '@' /tt3projeto/email.txt; CONTEUDO=$?; f2;;
+                1) grep -q '@' /tt3/email.txt; CONTEUDO=$?; f2;;
         esac
 fi
 }
 f2(){
 if [ $CONTEUDO == "0" ]; then
-        grep -q ' ' /tt3projeto/email.txt
+        grep -q ' ' /tt3/email.txt
         case $? in
                 0) echo "E-mail inválido"; sleep 3; email;;
                 1) final;;
@@ -57,23 +57,23 @@ else
 fi
 }
 final(){
-grep -q '\.com$' /tt3projeto/email.txt
+grep -q '\.com$' /tt3/email.txt
 case $? in
-        0) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "E-mail: $EMAIL" >> /tt3projeto/.dados/$NOME; fone;;
+        0) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "E-mail: $EMAIL" >> /tt3/.dados/$NOME; fone;;
         1) final2;;
 esac
 }
 final2(){
-grep -q '\.com\.br$' /tt3projeto/email.txt
+grep -q '\.com\.br$' /tt3/email.txt
 case $? in
-        0) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "E-mail: $EMAIL" >> /tt3projeto/.dados/$NOME; fone;;
+        0) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "E-mail: $EMAIL" >> /tt3/.dados/$NOME; fone;;
         1) final3;;
 esac
 }
 final3(){
-grep -q '\.br$' /tt3projeto/email.txt
+grep -q '\.br$' /tt3/email.txt
 case $? in
-        0) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "E-mail: $EMAIL" >> /tt3projeto/.dados/$NOME; fone;;
+        0) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "E-mail: $EMAIL" >> /tt3/.dados/$NOME; fone;;
         1) echo "E-mail inválido"; sleep 3; email;;
 esac
 }
@@ -82,18 +82,18 @@ clear
 echo $NOME
 echo "Telefone"
 read -n2 -p"(" DDD; read -n4 -p") " PR; read -n5 -p- SU; echo
-echo $PR > /tt3projeto/ddd.txt
-grep -q $DDD /tt3projeto/dddbrasil.txt
+echo $PR > /tt3/ddd.txt
+grep -q $DDD /tt3/dddbrasil.txt
 case $? in
         0) phon;;
         1) echo "DDD Não encontrado"; sleep 3; fone;;
 esac
 }
 phon(){
-grep -q -i '[a-z]' /tt3projeto/ddd.txt
+grep -q -i '[a-z]' /tt3/ddd.txt
 case $? in
         0) echo "Número inválido"; sleep 3; fone;;
-        1) grep -q '^0' /tt3projeto/ddd.txt; CONTEUDO=$?; ph;;
+        1) grep -q '^0' /tt3/ddd.txt; CONTEUDO=$?; ph;;
 esac
 }
 ph(){
@@ -102,11 +102,11 @@ if [ $CONTEUDO == "0" ]; then
         sleep 3
         fone
 else
-        echo $SU > /tt3projeto/ddd.txt
-        grep -q -i '[a-z]' /tt3projeto/ddd.txt
+        echo $SU > /tt3/ddd.txt
+        grep -q -i '[a-z]' /tt3/ddd.txt
         case $? in
                 0) echo "Número inválido"; sleep 3; fone;;
-                1) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "Telefone: ($DDD) $PR-$SU" >> /tt3projeto/.dados/$NOME; rg;;
+                1) echo "."; sleep 1; echo "."; sleep 1; echo "."; sleep 1; echo "Telefone: ($DDD) $PR-$SU" >> /tt3/.dados/$NOME; rg;;
         esac
 fi
 }
